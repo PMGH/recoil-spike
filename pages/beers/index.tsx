05cc10cb11a55115d8a4953c91785bf589ce74f5
Peter McCready <peter.mccready@ooni.com>
2022-12-28 16:07:57 +0000

    fetch first 25 beers

--- pages/beers/index.tsx
+++ pages/beers/index.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const BeersPage = () => {
+  useEffect(() => {
+    // https://punkapi.com/documentation/v2
+    // limited to 25 items by default (without pagination)
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(res => res.json())
+      .then(beers => {
+        console.log({ beers });
+      })
+      .catch(error => {
+        console.error({ error });
+      });
+  });
+
   return (
     <div>
       Beers Page
